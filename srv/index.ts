1fa83b5f4107316ce7d5546af1871e84feaa401a
Enigmatrix <enigmatrix2000@gmail.com>
2019-06-23 13:38:42 +0800

    yeep

--- srv/index.ts
+++ srv/index.ts
@@ -2,7 +2,7 @@ import express, {Application, json} from 'express';
 
 const routes = async (path: string) => import(path).then((mod) => mod.default);
 
-export default (app: Application, http) => {
+export default async (app: Application) => {
   app.use(json());
 
   app.use('/api/chall', await routes('./routes/chall'));
