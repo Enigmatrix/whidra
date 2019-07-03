8b31de88b5b11a186e37c5acd22b19f578b62194
Daniel Lim <weesoong.lim@gmail.com>
2019-07-03 08:27:54 +0000

    Change stuff in server.conf

--- ghidra/start.sh
+++ ghidra/start.sh
@@ -2,7 +2,10 @@
 
 cp server/hosts /etc/hosts
 
+server/ghidraSvr start
+server/ghidraSvr stop
 server/svrAdmin -add enigmatrix
 server/svrAdmin -add daniellimws
 server/svrAdmin -add ghidra
+server/svrAdmin -add root
 server/ghidraSvr console
