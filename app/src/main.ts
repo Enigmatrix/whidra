01147fcf0bae85269b85d2c3c7315ccc58eda869
Enigmatrix <enigmatrix2000@gmail.com>
2019-07-20 09:54:32 +0800

    add vuex

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
+import store from './store'
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: (h) => h(App),
+  store,
+  render: (h) => h(App)
 }).$mount('#app');
