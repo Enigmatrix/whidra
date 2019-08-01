ea0c9d3fdca7490c257effe51f4b9296d14052b5
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2019-08-01 21:55:38 +0800

    Give name for each monitor

--- ghidra/bridge/src/main.kt
+++ ghidra/bridge/src/main.kt
@@ -1,17 +1,34 @@
 import ghidra.GhidraJarApplicationLayout
 import ghidra.framework.Application
 import ghidra.framework.HeadlessGhidraApplicationConfiguration
+import ghidra.util.Msg
 import tool.*
 import util.RepositoryUtil.initServer
 
 
 fun main() {
     init()
-    val monitor = WhidraTaskMonitor()
-    val program = ImportBinaryTool.importBinary("testRepo", "bins/heapschool", monitor)
-    AutoAnalysisTool.analyzeProgram(program, monitor)
+
+    Msg.info("Main", "Start: Import Binary")
+    val program = ImportBinaryTool.importBinary("testRepo", "bins/heapschool", WhidraTaskMonitor("Import Binary"))
+    Msg.info("Main>", "End: Import Binary")
+
+    Msg.info("Main", "Start: Analyze Program")
+    AutoAnalysisTool.analyzeProgram(program, WhidraTaskMonitor("Analyze Program"))
+    Msg.info("Main", "End: Analyze Program")
+
+    Msg.info("Main", "Start: Disassemble")
     DisassemblerTool.disassemble(program, 0x400e05)
-    CommitProgramTool.commitProgram("testRepo", program, "Add ${program.name} to version control", monitor)
+    Msg.info("Main", "End: Disassemble")
+
+    Msg.info("Main", "Start: Commit Program")
+    CommitProgramTool.commitProgram(
+        "testRepo",
+        program,
+        "Add ${program.name} to version control",
+        WhidraTaskMonitor("Commit Program")
+    )
+    Msg.info("Main", "End: Commit Program")
 }
 
 fun init() {
