70cfe2821301688a8ce604e0224705399682eb06
cexplr <code.explore.95@gmail.com>
2019-08-03 10:28:32 +0800

    took away unnecessary  &&\ and commented out a line from Dockerfile to make ghidraserver work; Added myself in start.sh

--- ghidra/server/start.sh
+++ ghidra/server/start.sh
@@ -4,6 +4,7 @@ server/ghidraSvr start
 server/ghidraSvr stop
 server/svrAdmin -add enigmatrix
 server/svrAdmin -add daniellimws
+server/svrAdmin -add ubuntu-dev
 server/svrAdmin -add ghidra
 server/svrAdmin -add root
 server/ghidraSvr console
