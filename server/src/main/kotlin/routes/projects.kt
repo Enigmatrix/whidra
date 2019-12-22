5669b2fb8156c261eadd30f795945b6c9c17decf
Enigmatrix <enigmatrix2000@gmail.com>
2019-12-22 16:33:41 +0800

    proper projects.kt

--- server/src/main/kotlin/routes/projects.kt
+++ server/src/main/kotlin/routes/projects.kt
@@ -21,6 +21,7 @@ class Project {
     class All
 }
 
+// TODO get rid of these stupid wartings proejct-wide
 fun Route.projects() {
     post<Project.Create> {
         val server = call.whidraSession().server
@@ -39,15 +40,18 @@ fun Route.projects() {
     get<Project.All> {
         val server = call.whidraSession().server
 
-        server.repositoryNames.map {
+        call.respond(server.repositoryNames.map {
             projectFrom(server.getRepository(it))
-        }
+        })
     }
 }
 
 fun projectFrom(repo: RepositoryAdapter): models.Project {
-    return models.Project(repo.name,
+    repo.connect()
+    val proj = models.Project(repo.name,
         binaries = repo.getItemList("/").map { Binary(it.name) })
+    repo.disconnect()
+    return proj
 }
 
 
