b567117f1f7068bce3595c8d938a97278fe0923d
Enigmatrix <enigmatrix2000@gmail.com>
2019-12-29 12:09:34 +0800

    proper 1st time setup for ghidra docker iamge

--- ghidra/custom/start.sh
+++ ghidra/custom/start.sh
@@ -1,6 +1,8 @@
-server/ghidraSvr start
-server/ghidraSvr stop
-
-server/svrAdmin -add ghidra
+GHIDRA_REPOSITORIES_PATH=/srv/repositories
+if [ ! -e "${GHIDRA_REPOSITORIES_PATH}/users" ]; then
+  mkdir -p "${GHIDRA_REPOSITORIES_PATH}/~admin"
+  echo "Creating user 'ghidra' with default password 'changeme'"
+  echo "-add ghidra" >> "${GHIDRA_REPOSITORIES_PATH}/~admin/adm.cmd"
+fi
 
 server/ghidraSvr console -u
