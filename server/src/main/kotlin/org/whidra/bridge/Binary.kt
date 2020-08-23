634497de67437478c133a18ce024c3f9a93cf1c1
Enigmatrix <enigmatrix2000@gmail.com>
2020-08-23 13:53:11 +0800

    remove micronaut

--- server/src/main/kotlin/org/whidra/bridge/Binary.kt
+++
@@ -1,9 +0,0 @@
-package org.whidra.bridge
-
-import ghidra.program.flatapi.FlatProgramAPI
-import ghidra.program.model.listing.*
-
-class Binary(val program: Program) {
-    val api = FlatProgramAPI(program)
-
-}
--- server/src/test/kotlin/org/whidra/test/hello.kt
+++
@@ -1,9 +0,0 @@
-package org.whidra.test
-
-import io.kotest.core.spec.style.StringSpec
-
-class HelloTest(): StringSpec({
-    "test the server is running" {
-        assert(true)
-    }
-})
