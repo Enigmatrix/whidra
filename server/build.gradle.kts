7c917cfc887ccdced80a113578e43c4d8a68e1d4
Enigmatrix <enigmatrix2000@gmail.com>
2020-11-14 20:30:22 +0800

    clean

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,8 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 group = "org.whidra"
 version = "1.0-SNAPSHOT"
 
-val ktorVersion = "1.4.0"
-
 plugins {
     kotlin("jvm") version "1.4.0"
     application
@@ -12,16 +10,11 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven { setUrl("https://jitpack.io")}
-    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("io.ktor:ktor-server-netty:$ktorVersion")
-    implementation("io.ktor:ktor-jackson:$ktorVersion")
     implementation("ch.qos.logback:logback-classic:1.2.3")
-    implementation("com.github.papsign:Ktor-OpenAPI-Generator:-SNAPSHOT")
 }
 
 tasks {
